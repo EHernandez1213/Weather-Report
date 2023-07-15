00fe8046f5467c7ff29808c5431151318b2d5d82
Whynotwynaut <125845043+EHernandez1213@users.noreply.github.com>
2023-07-15 12:24:28 -0700

    displayed data on page. created readme

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-# Weather-Report
+# Weather Report
+
+## Description
+
+This project was created because I wanted to create a site to give a five day weather forcast.
+What this does is it retrieves coordinates for a city inputed in the search bar or cities buttons using the Open Weather geo location API. Those coordinates are then used with the Weather API to retrieve the weather data which is displayed on the page.
+What I learned from project was how to perform fetch requests with third party APIs and how to traverse the dom to get the information needed.
+
+## Table of Contents
+
+[Installation](#installation)
+
+[Usage](#usage)
+
+[Credits](#credits)
+
+[License](#license)
+
+[Badge](#badge)
+
+[Contribute](#contribute)
+
+[Test](#test)
+
+[Questions](#questions)
+
+## Installation
+
+N/A
+
+## Usage
+
+To use this project you can enter a city name in the search bar and click the search button or click on one of the buttons with the preset citys. The weather the five day forcast will be given back for the city inputed.
+
+## Credits
+
+Attributions: Open Weather APIs
+
+## License
+
+Copyright (C) 2023 Elenilson Hernandez
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+## Badge
+
+![License](https://img.shields.io/badge/License-MIT-green)
+
+## Contribute
+
+There is currently no way to contribute.
+
+## Test
+
+There is currently no way to test this project.
+
+## Questions
+
+If you have any questions you can reach me at elenilson.hernandez1213@gmail.com or my Github account, https://github.com/EHernandez1213
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,20 @@ var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat='
 var locationUrl = 'http://api.openweathermap.org/geo/1.0/direct?q='
 var key = 'b144468b3136528575a9b2333a4bed23'
 var search = document.querySelector('#search')
+var atlanta = document.querySelector('#atlanta')
+var la = document.querySelector('#la')
+var ny = document.querySelector('#ny')
+var chicago = document.querySelector('#chicago')
+var houston = document.querySelector('#houston')
+var miami = document.querySelector('#miami')
+var sf = document.querySelector('#sf')
+var seattle = document.querySelector('#seattle')
+var currentCity = document.getElementById('currentCity')
+var day1 = document.getElementById('day_1')
+var day2 = document.getElementById('day_2')
+var day3 = document.getElementById('day_3')
+var day4 = document.getElementById('day_4')
+var day5 = document.getElementById('day_5')
 
 function getCoords(city) {
     fetch(locationUrl + city + '&appid=' + key)
@@ -9,8 +23,9 @@ function getCoords(city) {
             return response.json();
         })
         .then(function (location) {
-            console.log(location);
-            console.log(location[0].lat, location[0].lon)
+            currentCity.textContent = city
+            // console.log(location);
+            // console.log(location[0].lat, location[0].lon)
             getForcast(location[0].lat, location[0].lon)
         })
 }
@@ -24,11 +39,82 @@ function getForcast(lat, long) {
         })
         .then(function (weather) {
             console.log(weather);
+            var rawtemp1 = weather.list[0].main.temp
+            var hum1 = weather.list[0].main.humidity
+            var temp1 = convertTempature(rawtemp1)
+            day1.children[1].textContent = 'Tempature: ' + temp1
+            day1.children[2].textContent = 'Humidity: ' + hum1
+            day1.children[3].textContent = weather.list[0].weather[0].description
+
+            var rawtemp2 = weather.list[4].main.temp
+            var hum2= weather.list[4].main.humidity
+            var temp2 = convertTempature(rawtemp2)
+            day2.children[1].textContent = 'Tempature: ' + temp2
+            day2.children[2].textContent = 'Humidity: ' + hum2
+            day2.children[3].textContent = weather.list[4].weather[0].description
+
+            var rawtemp3 = weather.list[8].main.temp
+            var hum3 = weather.list[8].main.humidity
+            var temp3 = convertTempature(rawtemp3)
+            day3.children[1].textContent = 'Tempature: ' + temp3
+            day3.children[2].textContent = 'Humidity: ' + hum3
+            day3.children[3].textContent = weather.list[8].weather[0].description
+
+            var rawtemp4 = weather.list[12].main.temp
+            var hum4 = weather.list[12].main.humidity
+            var temp4 = convertTempature(rawtemp4)
+            day4.children[1].textContent = 'Tempature: ' + temp4
+            day4.children[2].textContent = 'Humidity: ' + hum4
+            day4.children[3].textContent = weather.list[12].weather[0].description
+
+            var rawtemp5 = weather.list[16].main.temp
+            var hum5 = weather.list[16].main.humidity
+            var temp5 = convertTempature(rawtemp5)
+            day5.children[1].textContent = 'Tempature: ' + temp5
+            day5.children[2].textContent = 'Humidity: ' + hum5
+            day5.children[3].textContent = weather.list[16].weather[0].description
         })
+}
+
+function convertTempature(temp) {
+    let tempF = (temp -273) *9/5 + 32
+return tempF        
 }
 
 search.addEventListener('click', function () {
     var city = document.querySelector('#City').value;
     console.log(city);
+    getCoords(city);
+})
+atlanta.addEventListener('click', function () {
+    var city = 'Atlanta'
+    getCoords(city);
+})
+la.addEventListener('click', function () {
+    var city = 'Los Angeles'
+    getCoords(city);
+})
+ny.addEventListener('click', function () {
+    var city = 'New York'
+    getCoords(city);
+})
+chicago.addEventListener('click', function () {
+    var city = 'Chicago'
+    getCoords(city);
+})
+houston.addEventListener('click', function () {
+    var city = 'Houston'
+    getCoords(city);
+})
+miami.addEventListener('click', function () {
+    var city = 'Miami'
+    getCoords(city);
+})
+sf.addEventListener('click', function () {
+    var city = 'San Francisco'
+    getCoords(city);
+})
+seattle.addEventListener('click', function () {
+    var city = 'Seattle'
     getCoords(city);
 })
